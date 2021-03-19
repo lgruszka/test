68164ac1ca534dcd879bc13bdc0e85a6ecd93d32
lgruszka <l.gruszka@sorter.pl>
2021-03-19 17:00:04 +0100

    init commit

---
+++ test/test.py
@@ -0,0 +1,12 @@
+from pprint import pprint
+
+def print_dict(slownik):
+    """
+    Print a dictionary in elegant way
+    Args:
+        slownik (dir): dictionary to print
+
+    Returns:
+        None    
+    """    
+    pprint(slownik)
